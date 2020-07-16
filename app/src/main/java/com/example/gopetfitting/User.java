6e4b8346fa7944e44090334d9ff5d9e3939726a4
Henry Jiang <henryjiang@Henrys-MacBook-Pro.local>
2020-07-15 19:55:23 -0700

    First draft of User class

--- app/src/main/java/com/example/gopetfitting/User.java
+++ app/src/main/java/com/example/gopetfitting/User.java
@@ -6,7 +6,7 @@ public class User {
 
     // Basic login info
     private UUID userId;
-    private UUID imageId;
+//    private UUID imageId;
     private String name;
     private String email;
 
@@ -16,7 +16,37 @@ public class User {
     private Double height;
     private Double weight;
     private Double targetWeight;
+//    private int targetCaloriesLoss;
+
+    // Intake
+    private int lostCalories;
+    private int caloriesIntake;
 
     // Activity
+    private UUID activityId;
+    private Time exerciseTime;
 
+    // Pet
+    private UUID petId;
+    private int coins;
+    private int checkInDays;
+
+
+    public User(String name, String email, int age, Sex sex, Double height, Double weight, Double targetWeight) {
+        this.userId = UUID.randomUUID();
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.sex = sex;
+        this.height = height;
+        this.weight = weight;
+        this.targetWeight = targetWeight;
+        this.lostCalories = 0;
+        this.caloriesIntake = 0;
+        this.activityId = UUID.randomUUID();
+        this.exerciseTime = new Time(0, 0, 0);
+        this.petId = UUID.randomUUID();
+        this.coins = 0;
+        this.checkInDays = 0;
+    }
 }
